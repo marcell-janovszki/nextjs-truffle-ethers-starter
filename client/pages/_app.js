b675e7bca002ada9070fc02c46c17d989bdfe66e
marcell-janovszki <marcell.janovszki@gmail.com>
2021-09-02 02:17:34 +0200

    included new providers

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,15 +2,23 @@ import "tailwindcss/tailwind.css"
 
 import Head from "next/head"
 
-import EthersProvider from "../providers/EthersProvider"
+import EthereumProvider from "../providers/EthereumProvider"
+import ContractProvider from "../providers/ContractProvider"
+import ValueProvider from "../providers/ValueProvider"
 
 function App({ Component, pageProps }) {
   return (
     <>
-    <Head><title>SimpleMessage.sol</title></Head>
-    <EthersProvider>
-      <Component {...pageProps} />
-    </EthersProvider>
+      <Head>
+        <title>SimpleMessage.sol</title>
+      </Head>
+      <EthereumProvider>
+        <ContractProvider>
+          <ValueProvider>
+            <Component {...pageProps} />
+          </ValueProvider>
+        </ContractProvider>
+      </EthereumProvider>
     </>
   )
 }
