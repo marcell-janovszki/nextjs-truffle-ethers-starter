bb9acc636b32162c259a1f0f5f2568e9a36fc680
marcell-janovszki <marcell.janovszki@gmail.com>
2021-09-02 02:18:39 +0200

    added components to test connecting to metamask

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,28 @@
-import { useEthers } from "../providers/EthersProvider"
+import { useEffect, useState } from "react"
 
-import Message from "../components/Message"
+import MetaMaskConnectButton from "../components/MetaMaskConnectButton"
+import UpdateMessageButton from "../components/UpdateMessageButton"
+
+import { useContract } from "../providers/ContractProvider"
 
 function HomePage() {
-  const { contracts } = useEthers()
+  const { contracts } = useContract()
+
+  const [message, setMessage] = useState()
+
+  useEffect(async () => {
+    const _message = await contracts[0].contract.message()
+    setMessage(_message)
+  }, [])
+  // when not commented out,
+  // it errors out without an address even though im just trying to read data and not send a transaction..
+
   return (
     <div className="flex flex-col items-center">
-      <div className="m-4"> Contract [SimpleMessage.sol]</div>
-      <Message contract={contracts.SimpleMessage} />
+      home_page
+      <MetaMaskConnectButton />
+      {message ? message : null}
+      <UpdateMessageButton />
     </div>
   )
 }
